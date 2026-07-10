f03446ee3c96056104aaf4d72c4b9b22a92c4771
José Victor <josevictoroliveira160@gmail.com>
2026-07-09 22:37:10 -0300

    Update sequencer.py

--- app/sequencer/sequencer.py
+++ app/sequencer/sequencer.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 # Endereço do Sequenciador:
-HOST = '127.0.0.1'
+HOST = '0.0.0.0'
 PORT = 9000
 
 class Sequenciador:
@@ -14,15 +14,22 @@ class Sequenciador:
     def iniciar(self):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.settimeout(1)
         server_socket.bind((HOST, PORT))
         server_socket.listen()
+        
         print(f"[SEQUENCIADOR] Rodando em {HOST}:{PORT}...")
 
         while True:
-            conn, addr = server_socket.accept()
-            with self.lock:
-                self.clientes.append(conn)
-            threading.Thread(target=self.tratar_cliente, args=(conn,), daemon=True).start()
+            try:
+                conn, addr = server_socket.accept()
+                with self.lock:
+                    self.clientes.append(conn)
+                threading.Thread(target=self.tratar_cliente, args=(conn,), daemon=True).start()
+            except socket.timeout:
+                pass
+
+
 
     def tratar_cliente(self, conn):
         while True:
